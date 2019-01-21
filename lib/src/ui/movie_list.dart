9990af0b0f0067b73f8c8978ab4dda98343694ea
AndX2 <andrew.android@yandex.ru>
2019-01-22 00:07:05 +0300

    init commit

---
+++ lib/src/ui/movie_list.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'dart:async';
+
+import '../models/item_model.dart';
+import '../bloc/movies_bloc.dart';
+
+class MovieList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    bloc.fetchAllMovies();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Popular Movies'),
+      ),
+      body: StreamBuilder(
+        stream: bloc.allMovies,
+        builder: (context, AsyncSnapshot<ItemModel> snapshot) {
+          if (snapshot.hasData) {
+            return buildList(snapshot);
+          } else if (snapshot.hasError) {
+            return Text(snapshot.error.toString());
+          }
+          return Center(child: CircularProgressIndicator());
+        },
+      ),
+    );
+  }
+
+  Widget buildList(AsyncSnapshot<ItemModel> snapshot) {
+    return GridView.builder(
+        itemCount: snapshot.data.results.length,
+        gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            // mainAxisSpacing: 8.0,
+            // crossAxisSpacing: 8.0,
+            childAspectRatio: 0.7),
+        itemBuilder: (BuildContext context, int index) {
+          return Card(elevation: 8.0 ,child: GestureDetector(
+            child: imageNetworkWidgetWithErrorCatcher(() {
+              return Image.network(
+                'https://image.tmdb.org/t/p/w185${snapshot.data.results[index].posterPath}',
+                fit: BoxFit.cover,
+              );
+            }, context,
+                'https://image.tmdb.org/t/p/w185${snapshot.data.results[index].posterPath}'),
+            onTap: () {
+              print('Tap on index: $index');
+            },
+          ),);
+        });
+  }
+
+  Widget imageNetworkWidgetWithErrorCatcher(
+      Function function, BuildContext context, String url) {
+    bool isError = false;
+    precacheImage(NetworkImage(url), context, onError: (error, stackTrace) {
+      isError = true;
+    });
+    return isError
+        ? Center(
+            child: Text('No cover for this movie', maxLines: 2),
+          )
+        : function();
+  }
+}
